0078465e9de4297779489d63888bc9ba2b0bfbb1
momo <yangmomo314@gmail.com>
2020-03-12 17:21:10 +0800

    添加test工程到git中

--- git-test/src/main/java/com/msr/Hellogit.java
+++ git-test/src/main/java/com/msr/Hellogit.java
@@ -3,6 +3,7 @@ package com.msr;
 public class Hellogit {
     public static void main(String[] args) {
         System.out.println("111111111");
+        System.out.println("222222222222222");
     }
 }
 
